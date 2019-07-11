139d8fd2c5f904b965a2901a993ac8b6553967c0
Vitor Barros <vba@cesar.org.br>
2019-07-11 08:31:40 -0300

    conf: Include general html configuration
    This patch include three html flags.
    1- html_show_sphinx: remove "Created using Sphinx" in the html footer.
    2- html_show_sourcelink: remove "View source code" from sidebar. This
    option links do reST sources.
    3- html_scaled_image_link: remove links to the original images.
    
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- conf.py
+++ conf.py
@@ -71,3 +71,8 @@ html_logo = '_static/HeadLogo.svg'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_show_sphinx = False
+html_show_sourcelink = False
+
+html_scaled_image_link = False
